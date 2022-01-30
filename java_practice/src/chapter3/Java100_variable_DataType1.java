0258b338a5c49af664eca76b16e28b93fff7e6a2
jaeyeun <jaeyeun95@gmail.com>
2022-01-30 23:23:14 +0900

    예제로 공부하는 Java 100 문제풀이 part1 완료

---
+++ java_practice/src/chapter3/Java100_variable_DataType1.java
@@ -0,0 +1,25 @@
+package chapter3;
+
+public class Java100_variable_DataType1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c = 90;
+        double d;
+        char e;
+
+        a = 10;
+        b = 20;
+        d = 10.0;
+        e = 'A';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        short f = 'A';
+        System.out.println(f);
+    }
+}
